7c36ced006dacda975fead1369ad1f3e71b14469
Konstantin Konshu <konshu@addamant.ru>
2023-03-01 17:31:44 +0600

    feat(UI): добавлен BreadCrumbs

---
+++ src/shared/ui/BreadCrumbs/BreadCrumbs.styles.ts
@@ -0,0 +1,11 @@
+import { createStyles } from "@mantine/core";
+
+export default createStyles((_theme) => ({
+    root: {
+        flexWrap: "wrap",
+        rowGap: 20,
+    },
+    separator: {
+        marginInline: 8,
+    },
+}));
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,3 @@
-export * from './DataGrid'
-export * from './Forms'
+export * from "./DataGrid";
+export * from "./Forms";
+export * from "./BreadCrumbs";
