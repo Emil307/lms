d437aaa35f9d465cd2339848ba4e4f8eb9169bf9
Konstantin Konshu <konshu@addamant.ru>
2023-03-02 15:10:57 +0600

    feat(UI): добавлен Slider

---
+++ src/shared/ui/Forms/Slider/index.ts
@@ -0,0 +1,6 @@
+import Slider from "./Slider";
+import FSlider from "./FSlider";
+import type { SliderProps } from "./Slider";
+import type { FSliderProps } from "./FSlider";
+
+export { Slider, FSlider, SliderProps, FSliderProps };
