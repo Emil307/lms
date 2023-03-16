4df81b7fa9a8359f1700c58c619bbbc2b9c9e043
Konstantin <konsu@addamant.ru>
2023-03-16 05:35:28 +0000

    feat(UI): Input. Добавление инфоблоков, статусов и валид.ошибок согласно макетам

--- src/shared/ui/Forms/Input/Input.tsx
+++ src/shared/ui/Forms/Input/Input.tsx
@@ -1,14 +1,18 @@
-import { TextInput as MInput } from "@mantine/core";
-import { memo, ReactNode, useCallback, useState } from "react";
-import { Eye, EyeOff } from "react-feather";
+import { TextInput as MInput, Text, Group, useMantineTheme } from "@mantine/core";
+import { memo, ReactNode, useCallback, useMemo, useState } from "react";
+import { AlertTriangle, CheckCircle, Eye, EyeOff, Info } from "react-feather";
+import { z } from "zod";
 import { defaultTheme } from "@app/providers/Theme/theme";
 import { usePassword } from "@shared/utils/usePassword";
 import { useInputStyles } from "../../../styles/InputStyles";
 
-export interface InputProps extends React.ComponentProps<typeof MInput> {}
+export interface InputProps extends React.ComponentProps<typeof MInput> {
+    success?: string | boolean;
+}
 
-const MemoizedInput = memo(function Input(props: InputProps) {
+const MemoizedInput = memo(function Input({ success = false, error, description, ...props }: InputProps) {
     const { type, icon, rightSection, size } = props;
+    const theme = useMantineTheme();
 
     const isPasswordField = type === "password";
     const { isPasswordVisible, toggleVisibility } = usePassword();
@@ -34,11 +38,52 @@ const MemoizedInput = memo(function Input(props: InputProps) {
         return null;
     }, [getType()]);
 
+    const renderError = useMemo(
+        () =>
+            error && (
+                <>
+                    <AlertTriangle />
+                    <Text>{error}</Text>
+                </>
+            ),
+        [error]
+    );
+
+    const statusSuccess = useMemo(() => !!props.value && !error && !!success, [props.value, error, success]);
+
+    const renderDescription = useMemo(() => {
+        if (!description && !(success && statusSuccess)) {
+            return;
+        }
+
+        return (
+            <>
+                {statusSuccess && !z.boolean().safeParse(success).success && (
+                    <Group>
+                        <CheckCircle color={theme.colors.done[0]} />
+                        <Text>{success}</Text>
+                    </Group>
+                )}
+                {description && (
+                    <Group>
+                        <Info color={theme.colors.primaryHover[0]} />
+                        <Text>{description}</Text>
+                    </Group>
+                )}
+            </>
+        );
+    }, [statusSuccess, success, description]);
+
     const right = getRightSection();
 
     const [focused, setFocused] = useState(false);
 
-    const { classes } = useInputStyles({ floating: !!props.value || focused, icon: icon, size: size });
+    const { classes } = useInputStyles({
+        floating: !!props.value || focused,
+        icon,
+        size,
+        statusSuccess,
+    });
 
     return (
         <MInput
@@ -49,6 +94,9 @@ const MemoizedInput = memo(function Input(props: InputProps) {
             onBlur={() => setFocused(false)}
             rightSection={right}
             icon={icon}
+            inputWrapperOrder={["label", "input", "error", "description"]}
+            error={renderError}
+            description={renderDescription}
         />
     );
 });
