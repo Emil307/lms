5228de9a4e92a652a104aa9a706815c4dcb06bc5
Konstantin Konshu <konshu@addamant.ru>
2023-03-31 10:44:48 +0600

    feat(Пользовательская часть): Карточка для перехода к другим подборкам курсов

--- src/features/course-set/ui/index.ts
+++ src/features/course-set/ui/index.ts
@@ -1,3 +1,4 @@
 import CourseSetCard, { type CardProps as CourseSetCardProps } from "./Card";
+import CourseSetCardMore, { type CardMoreProps as CourseSetCardMoreProps } from "./CardMore";
 
-export { CourseSetCard, CourseSetCardProps };
+export { CourseSetCard, CourseSetCardMore, CourseSetCardProps, CourseSetCardMoreProps };
