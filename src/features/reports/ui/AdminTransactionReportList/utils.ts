3160557eabba6ca4a31248bd2fe441f7f4a0805d
Konstantin <konshu@addamant.ru>
2023-08-30 16:10:36 +0600

    refactor(Админка): Отчет по продажам. Обновление фильтров

--- src/features/reports/ui/AdminTransactionReportList/utils.ts
+++ src/features/reports/ui/AdminTransactionReportList/utils.ts
@@ -1,11 +1,11 @@
 import dayjs from "dayjs";
 import { TFunctionParamsWithoutPagination } from "@shared/ui/DataGrid/types";
-import { AdminStudentReportsFiltersForm, GetAdminStudentReportsRequest } from "@entities/report";
+import { AdminTransactionReportsFiltersForm, GetAdminTransactionReportsRequest } from "@entities/report";
 
 export const adaptGetAdminStudentReportsRequest = (
-    params: TFunctionParamsWithoutPagination<AdminStudentReportsFiltersForm>
-): GetAdminStudentReportsRequest => {
-    const { transactionableType, createdAtFrom, createdAtTo, roleId, transactionableIds, ...rest } = params;
+    params: TFunctionParamsWithoutPagination<AdminTransactionReportsFiltersForm>
+): GetAdminTransactionReportsRequest => {
+    const { transactionableType, createdAtFrom, createdAtTo, roleId, transactionableIds, paymentTypes, statuses, ...rest } = params;
 
     return {
         ...rest,
@@ -13,6 +13,12 @@ export const adaptGetAdminStudentReportsRequest = (
             transactionableType,
             transactionableIds,
             roleId,
+            ...(paymentTypes && {
+                paymentTypes: [paymentTypes],
+            }),
+            ...(statuses && {
+                statuses: [statuses],
+            }),
             date: {
                 from: dayjs(createdAtFrom).format("YYYY-MM-DD HH:mm:ss"),
                 to: dayjs(createdAtTo).endOf("day").format("YYYY-MM-DD HH:mm:ss"),
