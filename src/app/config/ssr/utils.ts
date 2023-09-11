7da8600cb2f9671033625a124658ad4896ac32f1
Chernikov Kirill <chernikov@addamant.ru>
2023-09-11 16:40:23 +0600

    fix(Общее): Вернул редирект на 404 и 500 страницы + поправлена обработка ошибок при ssr

--- src/app/config/ssr/utils.ts
+++ src/app/config/ssr/utils.ts
@@ -2,7 +2,7 @@ import { GetServerSidePropsContext } from "next";
 import Axios, { AxiosError } from "axios";
 import { getCookies } from "cookies-next";
 import { QueryClient } from "@tanstack/react-query";
-import { logoutPath } from "@app/routes";
+import {logoutPath} from "@app/routes";
 import { bindInterceptors } from "@app/config/axios/default";
 
 export async function getSsrInstances(context: GetServerSidePropsContext) {
@@ -25,20 +25,21 @@ export async function getSsrInstances(context: GetServerSidePropsContext) {
 export const handleAxiosErrorSsr = (errorSsr: unknown) => {
     const { response: error } = errorSsr as { response: AxiosError };
 
-    const isNetworkError = error.code === "ERR_NETWORK";
-
-    if (isNetworkError) {
-        return {
-            redirect: {
-                permanent: false,
-                destination: "/500",
-            },
-        };
-    }
+    // const isNetworkError = error.code === "ERR_NETWORK";
+    //
+    // if (isNetworkError) {
+    //     return {
+    //         redirect: {
+    //             permanent: false,
+    //             destination: "/500",
+    //         },
+    //     };
+    // }
 
     const statusCode = error.status;
     const isAccessError = statusCode === 403 || statusCode === 404;
     const isAuthError = statusCode === 401;
+    const isServerError = statusCode === 500;
 
     if (isAccessError) {
         return {
@@ -53,10 +54,12 @@ export const handleAxiosErrorSsr = (errorSsr: unknown) => {
             },
         };
     }
-    return {
-        redirect: {
-            permanent: false,
-            destination: "/500",
-        },
-    };
+    if (isServerError) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: "/500",
+            },
+        };
+    }
 };
