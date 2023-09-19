0d82f968ccd01d172973df828e7cca3fb6c2c78c
Konstantin <konshu@addamant.ru>
2023-09-19 10:10:43 +0600

    fix(Пользовательская часть): Прохождение ДЗ в уроке. Валидационная ошибка для текстового поля

--- src/features/lessons/ui/UpdateLessonHomeworkAnswerForm/types.ts
+++ src/features/lessons/ui/UpdateLessonHomeworkAnswerForm/types.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 import { $UploadedFile } from "@shared/types";
+import { REGEXP_TEXTEDITOR_INNER_TEXT } from "@shared/constant";
 
 export type UpdateLessonHomeworkAnswerFormValidation = z.infer<typeof $UpdateLessonHomeworkAnswerFormValidation>;
 
 export const $UpdateLessonHomeworkAnswerFormValidation = z.object({
-    answer: z.string(),
+    answer: z.string({ required_error: "Введите ответ" }).refine((value) => value.replace(REGEXP_TEXTEDITOR_INNER_TEXT, "").length, {
+        message: "Введите ответ",
+    }),
     files: $UploadedFile.array(),
 });
