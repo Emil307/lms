f39cd740f593b4129a9dd44fa05aab4c26a56a81
Konstantin <konsu@addamant.ru>
2023-09-25 07:33:49 +0000

    feat(Пользовательская часть): Плавающая кнопка фильтра с списке значений...

---
+++ src/features/courses/ui/Filters/components/FilterList/components/FilterItem/FilterItem.styles.ts
@@ -0,0 +1,33 @@
+import { createStyles } from "@mantine/core";
+
+interface CreateStylesParams {
+    selected?: boolean;
+}
+
+export default createStyles((theme, { selected }: CreateStylesParams) => ({
+    tooltip: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 48,
+        width: 48,
+        borderRadius: 8,
+        backgroundColor: theme.colors.white[0],
+        boxShadow: "0px 16px 32px 0px rgba(2, 6, 46, 0.08)",
+        pointerEvents: "auto",
+
+        ":hover": {
+            boxShadow: "0px 0px 16px 0px rgba(0, 18, 110, 0.04), 0px 1px 2px 0px rgba(0, 18, 110, 0.04)",
+        },
+    },
+    filterIcon: {
+        color: theme.colors.dark[0],
+        "&:hover": {
+            color: theme.colors.secondary[0],
+        },
+    },
+    tooltipInner: {
+        width: selected ? "calc(100% - 48px)" : "100%",
+        transition: "width 0.1s",
+    },
+}));
