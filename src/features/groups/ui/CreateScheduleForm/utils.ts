f72a20bd48fd73891801b398add9e15bbffa03a0
Chernikov Kirill <chernikov@addamant.ru>
2023-09-26 12:02:35 +0600

    fix(Админка): Фикс создания и обновления расписания группы

--- src/features/groups/ui/CreateScheduleForm/utils.ts
+++ src/features/groups/ui/CreateScheduleForm/utils.ts
@@ -4,11 +4,20 @@ import { CreateScheduleFormValidation } from "./types";
 
 export const adaptCreateGroupScheduleRequest = (data: CreateScheduleFormValidation): Omit<CreateAdminGroupScheduleRequest, "groupId"> => {
     const { scheduleDate, scheduleTimings = [] } = data;
+    const day = dayjs(scheduleDate).get("date");
+    const month = dayjs(scheduleDate).get("month");
+    const year = dayjs(scheduleDate).get("year");
+
     return {
         scheduleDate: dayjs(scheduleDate).format("YYYY-MM-DD"),
-        scheduleTimings: scheduleTimings.map((timing) => ({
-            from: dayjs(timing.from).format("YYYY-MM-DD HH:mm:ssZ"),
-            to: dayjs(timing.to).format("YYYY-MM-DD HH:mm:ssZ"),
-        })),
+        scheduleTimings: scheduleTimings.map((timing) => {
+            const from = dayjs(timing.from).set("date", day).set("month", month).set("year", year);
+            const to = dayjs(timing.to).set("date", day).set("month", month).set("year", year);
+
+            return {
+                from: dayjs(from).format("YYYY-MM-DD HH:mm:ssZ"),
+                to: dayjs(to).format("YYYY-MM-DD HH:mm:ssZ"),
+            };
+        }),
     };
 };
--- src/features/groups/ui/UpdateScheduleForm/utils.ts
+++ src/features/groups/ui/UpdateScheduleForm/utils.ts
@@ -21,12 +21,20 @@ export const adaptUpdateGroupScheduleRequest = (
     data: UpdateScheduleFormValidation
 ): Omit<UpdateAdminGroupScheduleRequest, "groupId" | "scheduleId"> => {
     const { scheduleDate, scheduleTimings = [] } = data;
+    const day = dayjs(scheduleDate).get("date");
+    const month = dayjs(scheduleDate).get("month");
+    const year = dayjs(scheduleDate).get("year");
+
     return {
         scheduleDate: dayjs(scheduleDate).format("YYYY-MM-DD"),
-        scheduleTimings: scheduleTimings.map((timing) => ({
-            ...(timing.id && { id: timing.id }),
-            from: dayjs(timing.from).utc().format("YYYY-MM-DD HH:mm:ssZ"),
-            to: dayjs(timing.to).utc().format("YYYY-MM-DD HH:mm:ssZ"),
-        })),
+        scheduleTimings: scheduleTimings.map((timing) => {
+            const from = dayjs(timing.from).set("date", day).set("month", month).set("year", year);
+            const to = dayjs(timing.to).set("date", day).set("month", month).set("year", year);
+
+            return {
+                from: dayjs(from).format("YYYY-MM-DD HH:mm:ssZ"),
+                to: dayjs(to).format("YYYY-MM-DD HH:mm:ssZ"),
+            };
+        }),
     };
 };
