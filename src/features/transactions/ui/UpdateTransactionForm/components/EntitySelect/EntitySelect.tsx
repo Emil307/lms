73a6bed4e94747905d27b62bde8b1b347a5d2aeb
Kirill <chernikov@addamant.ru>
2024-06-04 08:44:56 +0000

    fix(Админка): [108271] Фикс селекта сущностей в транзакциях

--- src/features/transactions/ui/UpdateTransactionForm/components/EntitySelect/EntitySelect.tsx
+++ src/features/transactions/ui/UpdateTransactionForm/components/EntitySelect/EntitySelect.tsx
@@ -1,24 +1,23 @@
 import { useDebouncedState } from "@mantine/hooks";
 import { useEffect } from "react";
 import { AdminTransactionEntity, TransactionEntityTypeName, useAdminTransactionCreateEntities } from "@entities/transaction";
-import { FSelect, prepareOptionsForSelect } from "@shared/ui";
+import { FSelect, SelectProps, prepareOptionsForSelect } from "@shared/ui";
 import { useIntersection } from "@shared/utils";
 import { initialParams } from "./constants";
 
-export interface EntitySelectProps {
+export interface EntitySelectProps extends Omit<SelectProps, "data"> {
     name: string;
     entityType: TransactionEntityTypeName;
     entity?: AdminTransactionEntity;
 }
 
-const EntitySelect = ({ name, entityType, entity }: EntitySelectProps) => {
+const EntitySelect = ({ name, entityType, entity, ...props }: EntitySelectProps) => {
     const [query, setQuery] = useDebouncedState("", 500);
 
     const {
         data: entitiesResourcesData,
         hasNextPage,
         fetchNextPage,
-        isLoading,
         isFetching,
         isRefetching,
     } = useAdminTransactionCreateEntities({ ...initialParams, query, entityType, filter: {} });
@@ -51,6 +50,7 @@ const EntitySelect = ({ name, entityType, entity }: EntitySelectProps) => {
     return (
         <>
             <FSelect
+                {...props}
                 name={name}
                 size="sm"
                 data={getOptions()}
@@ -58,7 +58,7 @@ const EntitySelect = ({ name, entityType, entity }: EntitySelectProps) => {
                 label="Сущность"
                 searchable
                 variantSearhableSelect="default"
-                disabled={isLoading || isFetching || !entityType}
+                disabled={!entityType}
                 lastElementRef={lastElemRef}
                 onSearchChange={setQuery}
                 maxDropdownHeight={150}
