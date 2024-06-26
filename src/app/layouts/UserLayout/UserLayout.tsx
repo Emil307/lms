c783bb61e1edbfa95169e46ac486bcb4a9820abc
Амина Кадырова <kadyrovaaminadev@gmail.com>
2024-06-26 08:01:58 +0000

    fix(общее/110739):  Для авторизованного пользователя Header сделать единым

--- src/app/layouts/UserLayout/UserLayout.tsx
+++ src/app/layouts/UserLayout/UserLayout.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mantine/core";
 import { AppShell } from "@mantine/core";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useScrollLock } from "@mantine/hooks";
 import { useRouter } from "next/router";
 import { FooterUser } from "@widgets/Footer";
@@ -10,6 +10,7 @@ import { isPathIncluded, publicPaths } from "@app/routes";
 import { useMedia } from "@shared/utils";
 import useStyles from "./UserLayout.styles";
 import { SidebarMenuContext } from "./utils";
+import { SessionContext } from "@app/providers/SessionProvider";
 
 export default function UserLayout({ children }: React.PropsWithChildren) {
     const [openedSidebar, setOpenedSidebar] = useState(false);
@@ -20,6 +21,10 @@ export default function UserLayout({ children }: React.PropsWithChildren) {
 
     const isMobile = useMedia("sm");
 
+    const ctx = useContext(SessionContext);
+
+    const isAuthorized = ctx.user?.id ? true : false;
+
     useEffect(() => {
         if (!isMobile && openedSidebar) {
             setOpenedSidebar(false);
@@ -29,20 +34,13 @@ export default function UserLayout({ children }: React.PropsWithChildren) {
 
     const isPublicPath = isPathIncluded(publicPaths, router.pathname);
 
-    const renderHeader = () => {
-        if (isPublicPath) {
-            return <HeaderPublicUser />;
-        }
-        return <HeaderUser />;
-    };
-
     return (
         <SidebarMenuContext.Provider value={{ openedSidebar, setOpenedSidebar }}>
             <AppShell
                 padding={16}
                 classNames={classes}
                 layout="alt"
-                header={renderHeader()}
+                header={isAuthorized ? <HeaderUser /> : <HeaderPublicUser />}
                 navbar={<NavbarUser hidden={!openedSidebar} isPublic={isPublicPath} />}
                 footer={<FooterUser hidden={openedSidebar} />}>
                 <Box className={classes.wrapperContent}>{children}</Box>
