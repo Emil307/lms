5db01beb71a1ed545eb5e304ae2879550ae3e48c
Kirill <chernikov@addamant.ru>
2024-08-27 12:05:31 +0600

    [111633] Общее / Проблема с next/fonts

--- src/app/providers/Theme/fonts.ts
+++ src/app/providers/Theme/fonts.ts
@@ -1,18 +1,9 @@
-import { Montserrat, Manrope, Inter } from "next/font/google";
 import localFont from "next/font/local";
 
-// Font files can be colocated inside of `pages`
-export const FuturaFont = localFont({
-    src: [
-        { path: "fonts/FuturaPT/FuturaPT-Bold.woff", style: "normal", weight: "bold" },
-        { path: "fonts/FuturaPT/FuturaPT-Book.woff", style: "normal", weight: "normal" },
-    ],
-});
-export const CentroSansPro = localFont({
-    src: [{ path: "fonts/FuturaPT/CenturyGothicPaneuropeanRegular.ttf", style: "normal", weight: "normal" }],
+export const ManropeFont = localFont({
+    src: [{ path: "fonts/Manrope/Manrope.woff2" }],
 });
 
-export const MontserratFont = Montserrat({ subsets: ["latin"] });
-export const ManropeFont = Manrope({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
-
-export const InterFont = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
+export const InterFont = localFont({
+    src: [{ path: "fonts/Inter/Inter.woff2" }],
+});
