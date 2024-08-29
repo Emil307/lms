bf77209502ae4ec092b387a9f06241a0245de4e8
Kirill <chernikov@addamant.ru>
2024-08-29 16:59:31 +0600

    [113954] Титульная страница / Создание отзыва / Ошибка CORS

--- src/pages/admin/settings/mainPage/AdvantagesPage/constants.ts
+++ src/pages/admin/settings/mainPage/AdvantagesPage/constants.ts
@@ -4,7 +4,6 @@ export const initialParams = {
 };
 
 export const tabsList = [
-    { id: 1, label: "Отзывы", value: "reviews" },
     { id: 2, label: "Главный баннер", value: "banner" },
     { id: 3, label: "Преимущества", value: "advantages" },
 ];
